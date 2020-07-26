5c3ea28f8e2668bdcaf7bfc35dec370751c5866d
KAMAN097 <63709039+KAMAN097@users.noreply.github.com>
2020-07-26 16:23:51 +0530

    finishing signin submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.style.scss";
 class SignIn extends React.Component {
   constructor(props) {
@@ -11,10 +11,15 @@ class SignIn extends React.Component {
       password: "",
     };
   }
-  handelSubmit = (event) => {
+  handelSubmit = async (event) => {
     event.preventDefault();
-
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
   handelChange = (e) => {
     const { name, value } = e.target;
